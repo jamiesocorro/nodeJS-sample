f4c7e9f95280dd2b4a126e53e8f26cccfaa393ff
jamiesocorro <jamie_socorro@live.com>
2018-09-11 02:45:14 -0600

    modified codes for testing

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
-//main module
+// main module
 
-console.log(module); // test the node module
+// test the node module
+console.log(module); 
 
-//use the logger method
-var logger = require('./logger'); 
+// use built in module
 
-logger.log('test this message');
+// directory
+const path = require('path');
+let pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+// operating system
+const os = require('os');
+let totalMemory = os.totalmem();
+
+console.log(`Total Memory: ${totalMemory}`);
+
+// file system
+const fs = require('fs');
+fs.readdir('./', function(err, files){
+    if(err) console.log('Error', err);
+    else console.log('Files', files);
+});
+
+// event 
+const EventEmitter = require('events');
+
+const Logger = require('./logger');
+const loggers = new Logger();
+
+// add a listener 
+loggers.on('logged', (arg) => {
+    console.log('called', arg);
+});
+
+loggers.log('log it');
+
+
--- logger.js
+++ logger.js
@@ -1,7 +1,19 @@
-//
+// module wrapped (function(exports, require, module, __filename, __dirname))
 
-function log(message) {
-    console.log(message);
+// event 
+const EventEmitter = require('events');
+
+console.log(__filename);
+
+class Logger extends EventEmitter {
+
+    log(message) {
+        console.log(message);
+
+        // signal
+        this.emit('logged', { id: 1, url: 'http://test.com' });
+    }
+
 }
 
-module.exports.log = log;
+module.exports = Logger;
